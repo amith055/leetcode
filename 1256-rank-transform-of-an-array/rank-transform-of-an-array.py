2b1fe0931d0e0f0b21a9069cc388b644dbbbc589
amith055 <153523730+amith055@users.noreply.github.com>
2026-07-12 20:10:03 +0530

    Time: 33 ms (82.61%) | Memory: 37.6 MB (60.61%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,6 +1,5 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        acpy = arr.copy()
         a = list(set(arr))
         a.sort()
         d = {val:rnk+1 for rnk,val in enumerate(a)}
