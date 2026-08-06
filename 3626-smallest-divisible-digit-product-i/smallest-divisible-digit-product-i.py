ec8b7bad6b9f7cb0ae6ce3f464ca515beab5a6ed
amith055 <153523730+amith055@users.noreply.github.com>
2026-08-06 12:02:45 +0530

    Time: 3 ms (17.48%) | Memory: 19.4 MB (32.93%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -4,11 +4,12 @@ class Solution:
 
         while True:
             smallest = n
-            n = n+1
+            
             prod = 1
             while smallest >0:
                 d = smallest % 10
                 prod *= d
                 smallest = smallest//10
             if prod % t == 0:
-                return n-1
+                return n
+            n = n+1
